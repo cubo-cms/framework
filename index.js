0bc9c23dc14e80cb575c1eeea7b5364f937647e8
papiando <info@papiando.com>
2020-09-19 23:21:21 -0400

    Simplified loading of classes

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ var path = require('path');
 global._module_ = path.resolve(__dirname);
 global._root_ = _module_.substring(0, _module_.indexOf('/node_modules') == -1? _module_.length : _module_.indexOf('/node_modules'));
 
-const framework = require('./lib');
-
-global.app = new framework.Application();
+global.Framework = require('./lib');
+global.app = new Framework.Application();
 
 console.log(app);
