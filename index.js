43f2f9ef7b8d9a2736dd3899e860bb3abdb1afb5
papiando <info@papiando.com>
2020-09-19 21:53:22 -0400

    index.js: fixing _root_

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-let Application = require('./lib/class/Application');
 
 // Get root path and store as global
 var path = require('path');
 global._module_ = path.resolve(__dirname);
-global._root_ = _module_.substring(0, _module_.indexOf('/node_modules'));
+global._root_ = _module_.substring(0, _module_.indexOf('/node_modules') == -1? _module_.length : _module_.indexOf('/node_modules'));
 
-global.app = new Application();
+const framework = require('./lib');
+
+global.app = new framework.Application();
+
+console.log(app);
