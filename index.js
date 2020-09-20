03b810fd41316987f6aeaa9d538dcfe01b74700e
papiando <info@papiando.com>
2020-09-20 03:43:08 -0400

    fixed router parsing

--- index.js
+++ index.js
@@ -4,7 +4,18 @@ var path = require('path');
 global._module_ = path.resolve(__dirname);
 global._root_ = _module_.substring(0, _module_.indexOf('/node_modules') == -1? _module_.length : _module_.indexOf('/node_modules'));
 
+// Get Framework classes
 global.Framework = require('./lib');
+
+// Run application
 global.app = new Framework.Application();
 
-console.log(app);
+//console.log(app);
+
+req = { "url": "https://papiando.com/user/papiando?format=json" };
+
+console.log(req.url);
+
+app.router.parse(req.url);
+
+console.log(app.router);
