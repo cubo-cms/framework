8982d1df51646e26f8a4e1df083f319ef905e637
papiando <info@papiando.com>
2020-10-11 17:14:29 -0400

    Update package.json

---
+++ index.mjs
@@ -0,0 +1,18 @@
+/** @package        @cubo-cms/framework
+  * @version        0.0.12
+  * @copyright      2020 Cubo CMS <https://cubo-cms.com/COPYRIGHT.md>
+  * @license        MIT license <https://cubo-cms.com/LICENSE.md>
+  * @author         Papiando <info@papiando.com>
+  * @description    Integrates packages by auto-registering all modules
+  **/
+
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+import Namespace from '@cubo-cms/core';
+
+const basePath = dirname(fileURLToPath(import.meta.url));
+
+Namespace.autoRegister('./lib', basePath);
+
+export default Namespace;
